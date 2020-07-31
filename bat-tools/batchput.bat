2d279e1abfda2dca85c364f6a217f4c7b626b034
fang_houjun <fanghoujun@126.com>
2020-07-31 08:03:24 +0800

    edit by fanghj

---
+++ bat-tools/batchput.bat
@@ -0,0 +1,2 @@
+put E:\business_platform\pcms_springcloud_20200526\bosent1611_pcms\bosent_baseline_hot-deploy-SPRINGCLOUD-pcmschannalcatalog\target\bosent_baseline_hot-deploy-SPRINGCLOUD-pcmschannalcatalog-161102-08-0.1-executable.jar  /opt/pcms-springcloud/pcms-channalcatalog 
+put E:\business_platform\pcms_springcloud_20200526\bosent1611_pcms\bosent_baseline_hot-deploy-SPRINGCLOUD-pcmsproductmix\target\bosent_baseline_hot-deploy-SPRINGCLOUD-pcmsproductmix-161102-08-0.1-executable.jar  /opt/pcms-springcloud/pcms-productmix 
